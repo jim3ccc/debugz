f4c4c855b0a7202b8daf8ec7c1185bd6c2593ff8
root <jimchiang001@gmail.com>
2017-07-11 07:22:17 +0800

    refactor app name and tested

---
+++ sample/src/main/java/com/jimdoesnotgym/ms076/logz/sample.java
@@ -0,0 +1,30 @@
+package com.jimdoesnotgym.ms076.logz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jimdoesnotgym.ms076.logs_android.Debugz;
+
+import java.util.Random;
+
+public class sample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        randomFunction();
+
+        System.out.println(Debugz.getStackTraceElement());
+        System.out.println(Debugz.getStackTraceElements());
+    }
+
+
+
+    public void randomFunction(){
+        Debugz.out(this.getClass().getSimpleName(), "Hello", true);
+        Random random = new Random();
+        System.out.println(random.nextInt());
+    }
+}
