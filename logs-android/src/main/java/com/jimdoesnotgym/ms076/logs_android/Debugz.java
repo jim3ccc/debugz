81489836b33468e0f5ebb094cf575bc2edda93b5
Jim Chiang <Jim.Chiang@tomtom.com>
2017-07-24 11:54:37 +0800

    allows different types of logs

--- logs-android/src/main/java/com/jimdoesnotgym/ms076/logs_android/Debugz.java
+++ logs-android/src/main/java/com/jimdoesnotgym/ms076/logs_android/Debugz.java
@@ -2,6 +2,8 @@ package com.jimdoesnotgym.ms076.logs_android;
 
 import android.util.Log;
 
+import com.jimdoesnotgym.ms076.logs_android.Exception.InvalidLogTypeException;
+
 public class Debugz {
 
     private static boolean mIsEnable;
@@ -10,12 +12,55 @@ public class Debugz {
         mIsEnable = isEnable;
     }
 
+    public enum Type{
+        V, E, I, W, D
+    }
+
+    public static void out(String tag, String message){
+        if(mIsEnable){
+            Log.d(tag, message);
+        }
+    }
+
     public static void out(String tag, String message, boolean showStackTrace){
         if(mIsEnable){
             Log.d(tag, message);
-            if(showStackTrace){
-                new Exception().printStackTrace();
+        }
+        if(showStackTrace){
+            new Exception().printStackTrace();
+        }
+    }
+
+    public static void out(String tag, String message, boolean showStackTrace, Type type) throws InvalidLogTypeException {
+        if(mIsEnable){
+            switch (type){
+                case V:
+                    Log.v(tag, message);
+                    break;
+
+                case E:
+                    Log.e(tag, message);
+                    break;
+
+                case I:
+                    Log.i(tag, message);
+                    break;
+
+                case W:
+                    Log.w(tag, message);
+                    break;
+
+                case D:
+                    Log.d(tag, message);
+                    break;
+
+                default:
+                    throw new InvalidLogTypeException("Invalid Log Type");
             }
+
+        }
+        if(showStackTrace){
+            new Exception().printStackTrace();
         }
     }
 
---
+++ logs-android/src/main/java/com/jimdoesnotgym/ms076/logs_android/Exception/InvalidLogTypeException.java
@@ -0,0 +1,8 @@
+package com.jimdoesnotgym.ms076.logs_android.Exception;
+
+
+public class InvalidLogTypeException extends Exception {
+    public InvalidLogTypeException(String message) {
+        super(message);
+    }
+}
